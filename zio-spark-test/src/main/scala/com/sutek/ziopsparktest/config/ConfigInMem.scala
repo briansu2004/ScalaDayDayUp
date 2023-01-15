94f78410296d83a4a2f3c72b9cbcb79cbf95971e
Brian Su <briansu2004@hotmail.com>
2023-01-15 10:33:56 -0500

    20230115: config fix

--- zio-spark-test/src/main/scala/com/sutek/ziopsparktest/config/ConfigInMem.scala
+++ zio-spark-test/src/main/scala/com/sutek/ziopsparktest/config/ConfigInMem.scala
@@ -12,7 +12,7 @@ final case class MyConfig(
 
 object ConfigInMem {
   //private val confLayer = TypesafeConfigSource.fromHoconString("enableAlert = true").toLayer
-  private val confLayer = TypesafeConfigSource.fromHoconFile(new File("c:/tmp/myconfig.conf")).toLayer
+  private val confLayer = TypesafeConfigSource.fromHoconFile(new File("src/main/resources/myconfig.conf")).toLayer
 
   // private val desc = descriptor[MyConfig]
   private val desc : ConfigDescriptor[MyConfig] = descriptor[MyConfig]
