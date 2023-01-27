3eb1b3d1235e8404efffdd49d0386fd1c7de789b
Weili Zhi <zhiwilliam@gmail.com>
2023-01-26 19:48:23 -0500

    Fixed.

--- zio-spark-test/src/main/scala/com/sutek/ziosparktest/config/ConfigInMem.scala
+++ zio-spark-test/src/main/scala/com/sutek/ziosparktest/config/ConfigInMem.scala
@@ -4,13 +4,15 @@ import zio.config._
 import zio.config.magnolia._
 import zio.config.typesafe.TypesafeConfigSource
 
+import java.io.File
+
 final case class MyConfig(
                           sourceCsvFilePath: String //Refined[String, NonEmpty]
                          )
 
 object ConfigInMem {
   //private val confLayer = TypesafeConfigSource.fromHoconString("enableAlert = true").toLayer
-  private val confLayer = TypesafeConfigSource.fromHoconFile(new File("src/main/resources/myconfig.conf")).toLayer
+  private val confLayer = TypesafeConfigSource.fromHoconFile(new File("C:\\Users\\zhiwi\\code\\ScalaDayDayUp\\zio-spark-test\\src\\main\\resources\\myconfig.conf")).toLayer
 
   // private val desc = descriptor[MyConfig]
   private val desc : ConfigDescriptor[MyConfig] = descriptor[MyConfig]
