65156ea74a06443c019a6701d36172dcbebf658e
Brian Su <briansu2004@hotmail.com>
2023-01-28 09:18:41 -0500

    $

--- zio-spark-test/src/main/scala/com/sutek/ziosparktest/config/ConfigInMem.scala
+++ zio-spark-test/src/main/scala/com/sutek/ziosparktest/config/ConfigInMem.scala
@@ -1,18 +1,28 @@
 package com.sutek.ziosparktest.config
 
+import com.sutek.ziosparktest.io.ReadFile._
 import zio.config._
 import zio.config.magnolia._
 import zio.config.typesafe.TypesafeConfigSource
-import com.sutek.ziosparktest.io.ReadFile._
-import zio.ZLayer
 
 final case class MyConfig(
                           sourceCsvFilePath: String //Refined[String, NonEmpty]
                          )
 
 object ConfigInMem {
-  //private val confLayer = TypesafeConfigSource.fromHoconFile(new File("c:/tmp/myconfig.conf")).toLayer
+  // private val confLayer = TypesafeConfigSource.fromHoconFile(new File("c:/tmp/myconfig.conf")).toLayer
 
   // readFileLinesStream("c:/tmp/myconfig.conf").runCollect.map(_.mkString("\n"))
-  val live = ZLayer.fromZIO(readFileLines("c:/tmp/myconfig.conf").map(TypesafeConfigSource.fromHoconString)) >>> configLayer(descriptor[MyConfig])
+
+  //  val live = ZLayer.fromZIO(readFileLines("src/main/resources/myconfig.conf").map(TypesafeConfigSource
+  //    .fromHoconString)) >>> configLayer(descriptor[MyConfig])
+
+  val hocon =
+    s"""
+      {
+         sourceCsvFilePath : src/main/resources/data.csv
+      }
+    """
+
+  val live = TypesafeConfigSource.fromHoconString(hocon).toLayer >>> configLayer(descriptor[MyConfig])
 }
